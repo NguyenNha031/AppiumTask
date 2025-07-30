d59324716278f48d8717c399e33ece90f7c686b7
NguyenNha031 <nha.nt.64cntt@ntu.edu.vn>
2025-07-30 14:53:13 +0700

    Update workflow

--- SimpleAppium/Drivers/DriverFactory.cs
+++ SimpleAppium/Drivers/DriverFactory.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using System;
+using System.IO;
 
 namespace SimpleAppium.Helpers
 {
@@ -8,15 +9,19 @@ namespace SimpleAppium.Helpers
     {
         public static AndroidDriver CreateDriver()
         {
+            string basePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\.."));
+            string appPath = Path.Combine(basePath, "SimpleAppium", "SauceLabs-Demo-App.apk");
+            Console.WriteLine($"[DEBUG] App path: {appPath}");
+
             var options = new AppiumOptions
             {
                 PlatformName = "Android",
                 DeviceName = "emulator-5554",
                 AutomationName = "UiAutomator2",
-                App = @"D:\TestAppium\SauceLabs-Demo-App.apk"
+                App = appPath
             };
             options.AddAdditionalAppiumOption("autoGrantPermissions", true);
-            options.AddAdditionalAppiumOption("appWaitActivity", "com.saucelabs.mydemoapp.android.view.activities.MainActivity"); 
+            options.AddAdditionalAppiumOption("appWaitActivity", "com.saucelabs.mydemoapp.android.view.activities.MainActivity");
             options.AddAdditionalAppiumOption("fullReset", false);
 
             return new AndroidDriver(new Uri("http://127.0.0.1:4723"), options);
